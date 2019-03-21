0b443aa503ae9aee3a724144710e8a5501974b3e
jemurra <human_199@yahoo.com>
2019-03-21 11:09:16 -0700

    Add files via upload

---
+++ MerchMadness/merchm.js
@@ -0,0 +1,82 @@
+var merchm = [
+  {
+    "Per": "Ankle Breakers",
+    "Champ": "NC",
+    "Pts": 0
+  },
+  {
+    "Per": "Dan",
+    "Champ": "Gonzaga",
+    "Pts": 0
+  },
+  {
+    "Per": "DAWGS",
+    "Champ": "Gonzaga",
+    "Pts": 0
+  },
+  {
+    "Per": "Gary D",
+    "Champ": "Mich St",
+    "Pts": 0
+  },
+  {
+    "Per": "Gary M",
+    "Champ": "Duke",
+    "Pts": 0
+  },
+  {
+    "Per": "Go Dawgs",
+    "Champ": "Gonzaga",
+    "Pts": 0
+  },
+  {
+    "Per": "J's Stonecutters",
+    "Champ": "Gonzaga",
+    "Pts": 0
+  },
+  {
+    "Per": "Kelly",
+    "Champ": "Duke",
+    "Pts": 0
+  },
+  {
+    "Per": "Le Baguettes",
+    "Champ": "Montana",
+    "Pts": 0
+  },
+  {
+    "Per": "Madster",
+    "Champ": "",
+    "Pts": 0
+  },
+  {
+    "Per": "Mark",
+    "Champ": "Kentuky",
+    "Pts": 0
+  },
+  {
+    "Per": "Mitch",
+    "Champ": "Tennessee",
+    "Pts": 0
+  },
+  {
+    "Per": "Nick",
+    "Champ": "North Carolina",
+    "Pts": 0
+  },
+  {
+    "Per": "The Snap",
+    "Champ": "North Carolina",
+    "Pts": 0
+  },
+  {
+    "Per": "Tobias",
+    "Champ": "Seton Hall",
+    "Pts": 0
+  },
+  {
+    "Per": "Word Wheelers",
+    "Champ": "Duke",
+    "Pts": 0
+  }
+]
