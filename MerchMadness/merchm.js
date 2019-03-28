e0aa03f1696c78dc21086e6b858a78477fc92660
jemurra <human_199@yahoo.com>
2019-03-28 14:17:43 -0700

    Update merchm.js

--- MerchMadness/merchm.js
+++ MerchMadness/merchm.js
@@ -2,81 +2,113 @@ var merchm = [
   {
     "Per": "Ankle Breakers",
     "Champ": "NC",
-    "Pts": 49
+    "Pts": 49,
+    "R1": 27,
+    "R2": 22
   },
   {
     "Per": "Dan",
     "Champ": "Gonzaga",
-    "Pts": 45
+    "Pts": 45,
+    "R1": 25,
+    "R2": 20
   },
   {
     "Per": "DAWGS",
     "Champ": "Gonzaga",
-    "Pts": 40
+    "Pts": 40,
+    "R1": 22,
+    "R2": 18
   },
   {
     "Per": "Gary D",
     "Champ": "Mich St",
-    "Pts": 54
+    "Pts": 54,
+    "R1": 24,
+    "R2": 30
   },
   {
     "Per": "Gary M",
     "Champ": "Duke",
-    "Pts": 32
+    "Pts": 32,
+    "R1": 22,
+    "R2": 10
   },
   {
     "Per": "Go Ducks",
     "Champ": "Gonzaga",
-    "Pts": 46
+    "Pts": 46,
+    "R1": 26,
+    "R2": 20
   },
   {
     "Per": "J's Stonecutters",
     "Champ": "Gonzaga",
-    "Pts": 38
+    "Pts": 38,
+    "R1": 20,
+    "R2": 18
   },
   {
     "Per": "Kelly",
     "Champ": "Duke",
-    "Pts": 51
+    "Pts": 51,
+    "R1": 27,
+    "R2": 24
   },
   {
     "Per": "Le Baguettes",
     "Champ": "Montana",
-    "Pts": 29
+    "Pts": 29,
+    "R1": 17,
+    "R2": 12
   },
   {
     "Per": "Madster",
     "Champ": "",
-    "Pts": 46
+    "Pts": 46,
+    "R1": 22,
+    "R2": 24
   },
   {
     "Per": "Mark",
     "Champ": "Kentuky",
-    "Pts": 48
+    "Pts": 48,
+    "R1": 24,
+    "R2": 24
   },
   {
     "Per": "Mitch",
     "Champ": "Tennessee",
-    "Pts": 39
+    "Pts": 39,
+    "R1": 23,
+    "R2": 16
   },
   {
     "Per": "Nick",
     "Champ": "North Carolina",
-    "Pts": 51
+    "Pts": 51,
+    "R1": 25,
+    "R2": 26
   },
   {
     "Per": "The Snap",
     "Champ": "North Carolina",
-    "Pts": 41
+    "Pts": 41,
+    "R1": 21,
+    "R2": 20
   },
   {
     "Per": "Tobias",
     "Champ": "Seton Hall",
-    "Pts": 31
+    "Pts": 31,
+    "R1": 21,
+    "R2": 10
   },
   {
     "Per": "Word Wheelers",
     "Champ": "Duke",
-    "Pts": 44
+    "Pts": 44,
+    "R1": 22,
+    "R2": 22
   }
 ]
