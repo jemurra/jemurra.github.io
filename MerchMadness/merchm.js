9ab350f231b892d10a345330e4ce9219ebaaa4aa
jemurra <human_199@yahoo.com>
2019-03-29 09:14:12 -0700

    Update merchm.js

--- MerchMadness/merchm.js
+++ MerchMadness/merchm.js
@@ -4,111 +4,127 @@ var merchm = [
     "Champ": "NC",
     "Pts": 49,
     "R1": 27,
-    "R2": 22
+    "R2": 22,
+    "R3": 9
   },
   {
     "Per": "Dan",
     "Champ": "Gonzaga",
     "Pts": 45,
     "R1": 25,
-    "R2": 20
+    "R2": 20,
+    "R3": 3
   },
   {
     "Per": "DAWGS",
     "Champ": "Gonzaga",
     "Pts": 40,
     "R1": 22,
-    "R2": 18
+    "R2": 18,
+    "R3": 9
   },
   {
     "Per": "Gary D",
     "Champ": "Mich St",
     "Pts": 54,
     "R1": 24,
-    "R2": 30
+    "R2": 30,
+    "R3": 6
   },
   {
     "Per": "Gary M",
     "Champ": "Duke",
     "Pts": 32,
     "R1": 22,
-    "R2": 10
+    "R2": 10,
+    "R3": 6
   },
   {
     "Per": "Go Ducks",
     "Champ": "Gonzaga",
     "Pts": 46,
     "R1": 26,
-    "R2": 20
+    "R2": 20,
+    "R3": 6
   },
   {
     "Per": "J's Stonecutters",
     "Champ": "Gonzaga",
     "Pts": 38,
     "R1": 20,
-    "R2": 18
+    "R2": 18,
+    "R3": 3
   },
   {
     "Per": "Kelly",
     "Champ": "Duke",
     "Pts": 51,
     "R1": 27,
-    "R2": 24
+    "R2": 24,
+    "R3": 6
   },
   {
     "Per": "Le Baguettes",
     "Champ": "Montana",
     "Pts": 29,
     "R1": 17,
-    "R2": 12
+    "R2": 12,
+    "R3": 0
   },
   {
     "Per": "Madster",
     "Champ": "",
     "Pts": 46,
     "R1": 22,
-    "R2": 24
+    "R2": 24,
+    "R3": 6
   },
   {
     "Per": "Mark",
     "Champ": "Kentuky",
     "Pts": 48,
     "R1": 24,
-    "R2": 24
+    "R2": 24,
+    "R3": 6
   },
   {
     "Per": "Mitch",
     "Champ": "Tennessee",
     "Pts": 39,
     "R1": 23,
-    "R2": 16
+    "R2": 16,
+    "R3": 6
   },
   {
     "Per": "Nick",
     "Champ": "North Carolina",
     "Pts": 51,
     "R1": 25,
-    "R2": 26
+    "R2": 26,
+    "R3": 6
   },
   {
     "Per": "The Snap",
     "Champ": "North Carolina",
     "Pts": 41,
     "R1": 21,
-    "R2": 20
+    "R2": 20,
+    "R3": 3
   },
   {
     "Per": "Tobias",
     "Champ": "Seton Hall",
     "Pts": 31,
     "R1": 21,
-    "R2": 10
+    "R2": 10,
+    "R3": 0
   },
   {
     "Per": "Word Wheelers",
     "Champ": "Duke",
     "Pts": 44,
     "R1": 22,
-    "R2": 22
+    "R2": 22,
+    "R3": 9
   }
 ]
